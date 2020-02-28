d7f94f0fc5f1f5e57325e2053c26a56da9a127ac
Freen <aboyinsky@outlook.com>
2020-02-28 15:05:48 +0800

    回调、不删除bookmarks表的id和添加bags-of-title文件的id

--- source/web/op_api.py
+++ source/web/op_api.py
@@ -18,8 +18,8 @@ class dat_api():
         try:
             #acquire the 2000 rows data
             self.bookmarks = pd.read_csv(bookmarks_path, engine='python', nrows=2000, sep="\t", encoding='utf-8')
-            # bookmarks'header is ['id','md5','title','url','md5Principal','urlPrincipal'],del row 'md5','md5Principal'
-            self.bookmarks = self.bookmarks.drop(['id','md5','url','md5Principal'],axis=1)
+            # bookmarks'header is ['id','md5','title','url','md5Principal','urlPrincipal']
+            self.bookmarks = self.bookmarks.drop(['md5','url','md5Principal'],axis=1)
             #将Title中的一些简单字符作预处理
             pattern = re.compile(r'[^a-zA-Z]')
             title_map = {val:re.sub(pattern, ' ', val) for ii,val in enumerate(set(self.bookmarks['title'])) }
@@ -32,10 +32,10 @@ class dat_api():
                         genres_set[i]=1
                     else:
                         genres_set[i]+=1
-            data = pd.DataFrame({'word': [val for val in genres_set.keys()],'counts': [val for val in genres_set.values()]})
+            data = pd.DataFrame({'id': [i for i in range(len(genres_set)) ],'word': [val for val in genres_set.keys()],'counts': [val for val in genres_set.values()]})
             print(data)
             # 写入文件/save/bookmarks.dat  运行一次就好了
-            # data.to_csv(os.path.dirname(__file__)+'/save/bookmarks.dat', sep=' ', header=True, index=True)
+            data.to_csv(os.path.dirname(__file__)+'/save/bags_of_title.dat', sep=' ', header=True, index=True)
         except pd.errors.ParserError as e:
             print("connect error|pandas Error: %s" % e)
 
