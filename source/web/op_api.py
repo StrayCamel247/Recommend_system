7dbc7cda1c7da401039859f796573682712ac19f
Freen <aboyinsky@outlook.com>
2020-02-28 14:56:29 +0800

    bags-of-words 生成新表 bookmarks.dat

--- source/web/op_api.py
+++ source/web/op_api.py
@@ -19,12 +19,23 @@ class dat_api():
             #acquire the 2000 rows data
             self.bookmarks = pd.read_csv(bookmarks_path, engine='python', nrows=2000, sep="\t", encoding='utf-8')
             # bookmarks'header is ['id','md5','title','url','md5Principal','urlPrincipal'],del row 'md5','md5Principal'
-            self.bookmarks = self.bookmarks.drop(['md5','url','md5Principal'],axis=1)
+            self.bookmarks = self.bookmarks.drop(['id','md5','url','md5Principal'],axis=1)
             #将Title中的一些简单字符作预处理
             pattern = re.compile(r'[^a-zA-Z]')
             title_map = {val:re.sub(pattern, ' ', val) for ii,val in enumerate(set(self.bookmarks['title'])) }
             self.bookmarks['title'] = self.bookmarks['title'].map(title_map)
-            print(title_map)
+            genres_set = dict()
+            
+            for val in self.bookmarks['title'].str.split(' +'):
+                for i in val:
+                    if i not in genres_set:
+                        genres_set[i]=1
+                    else:
+                        genres_set[i]+=1
+            data = pd.DataFrame({'word': [val for val in genres_set.keys()],'counts': [val for val in genres_set.values()]})
+            print(data)
+            # 写入文件/save/bookmarks.dat  运行一次就好了
+            # data.to_csv(os.path.dirname(__file__)+'/save/bookmarks.dat', sep=' ', header=True, index=True)
         except pd.errors.ParserError as e:
             print("connect error|pandas Error: %s" % e)
 
