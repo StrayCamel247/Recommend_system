3eee1ff4831d1bc68875fde66561fc702c26126c
Freen <aboyinsky@outlook.com>
2020-04-04 15:56:11 +0800

    annotations

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ def main(net_model, m_version):
     model = net_model.Net_works(256,10)
     history = model_history(model, m_version)
     mat_show.history_show_loss(history, m_version)
+
+# 开始训练前请先解压Data中的BX-CSV-Dump数据集
 # 第一次训练会缓存训练结果到tmp文件夹，如果更改数据重新训练请删除对应的history文件，再运行
 
 # main(model_0, 0)
 main(model_1, 1)
+# model_2 会报错 修复ing
 # main(model_2, 2)
